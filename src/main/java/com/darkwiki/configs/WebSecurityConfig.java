633b860c56f09bf7dee5b04c90217d909f0f1cc3
Chris <chris.baumgartner115@gmail.com>
2024-05-08 13:14:57 +0200

    All pages secured with login if not whitelisted.

--- src/main/java/com/darkwiki/configs/WebSecurityConfig.java
+++ src/main/java/com/darkwiki/configs/WebSecurityConfig.java
@@ -1,18 +1,14 @@
 package com.darkwiki.configs;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -20,42 +16,28 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    //    private static final String ADMIN = UserxRole.ADMIN.name();
-    private static final String LOGIN = "/login.xhtml";
-    private static final String ACCESSDENIED = "/error/access_denied.xhtml";
-
-    @Autowired
-    DataSource dataSource;
-
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        try {
-            http.cors(cors -> cors.disable()).csrf(csrf -> csrf.disable()).headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
-                    .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll()
-                    ).formLogin(form -> form.disable()
-                    ).logout(logout -> logout.disable()
-                    ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    );
-
-            return http.build();
-        } catch (Exception ex) {
-            throw new BeanCreationException("Wrong spring security configuration", ex);
-        }
+        return http
+                .authorizeRequests(
+                        authorize -> authorize
+                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/landing-page/index.html", true)
+                )
+                .build();
     }
 
-
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        //Configure roles and passwords via datasource
-//        auth.jdbcAuthentication().dataSource(dataSource)
-//                .usersByUsernameQuery("select username, password, enabled from userx where username=?")
-//                .authoritiesByUsernameQuery("select userx_username, roles from userx_userx_role where userx_username=?");
-//    }
-
     @Bean
-    public static PasswordEncoder passwordEncoder() {
-        // :TODO: use proper passwordEncoder and do not store passwords in plain text
-        return NoOpPasswordEncoder.getInstance();
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.builder()
+                .username("darkwiki")
+                .password(encoder.encode("zelenogradsky"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
